88cf8b25622e2b4e5cc6a7dfcc2dc0123d35be1a
MHabiburRahman <George_the@ymail.com>
2016-01-25 19:49:30 +0700

    no message

--- BubbleRig2/Assets/Script/CurvePipe.cs
+++ BubbleRig2/Assets/Script/CurvePipe.cs
@@ -4,13 +4,36 @@ using System.Collections;
 public class CurvePipe : MonoBehaviour {
 
 	public int count = 0;
-	public int stat = 0;
-	
+	public bool right = false;
+	public GameObject[] turns;
+	public Transform[] belok;
+
+	void Start(){
+	}
+
+	void Update(){
+		if (count%2==0) {
+			turns[0].transform.position = belok[0].transform.position;
+			turns[1].transform.position = belok[1].transform.position;
+			if(turns[0].transform.position == belok[0].position){
+				print("InPosition turns 0");
+			}
+		} 
+		else {
+			turns[1].transform.position = belok[0].transform.position;
+			turns[0].transform.position = belok[1].transform.position;
+			if(turns[1].transform.position == belok[0].position){
+				print("InPosition turns 1");
+			}
+		}
+	}
+
 	void OnMouseDown () {
-		if (count % 2 == 0)
-			transform.localScale = new Vector3(transform.localScale.x , transform.localScale.y ,transform.localScale.z);
+		if(count % 2==0 )
+			transform.localScale = new Vector3 (transform.localScale.x, transform.localScale.y, transform.localScale.z);
 		else
-			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y ,transform.localScale.z);
+			transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
 		count++;
 	}
 }
